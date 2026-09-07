ea787028272c24d03ec449cc7fa1d1ef7688b72f
oriyongfly <orionkibret@icloud.com>
2026-09-07 13:08:15 +0300

    home-ui1

--- tg-bot/src/pages/HomePage.jsx
+++ tg-bot/src/pages/HomePage.jsx
@@ -9,8 +9,6 @@ const HomePage = ({ user }) => {
   // Format user data from AuthContext
   const firstName = user?.first_name || 'Guest';
   const lastName = user?.last_name || '';
-  const username = user?.username || '';
-  const displayName = firstName + (lastName ? ` ${lastName}` : '');
   
   // Balance and coins - default values for now (will come from DB later)
   const displayBalance = user?.balance || 'ETB 0.00';
